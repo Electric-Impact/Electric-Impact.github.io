8eed953718f5349eaf0ba21a2da6682919715abc
Dan <dan.erikson@goldtowngames.com>
2026-02-15 12:50:47 +0100

    try again

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,21 @@
-const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
+const { VanillaExtractPlugin } = require("@vanilla-extract/webpack-plugin");
 
 module.exports = {
   babel: {
-    plugins: ['@vanilla-extract/babel-plugin'],
+    plugins: ["@vanilla-extract/babel-plugin"],
   },
   webpack: {
     configure: (webpackConfig) => {
+      // 🔴 remove CRA's "outside src" restriction
+      webpackConfig.resolve.plugins = webpackConfig.resolve.plugins.filter(
+        (plugin) => plugin.constructor.name !== "ModuleScopePlugin"
+      );
+
+      // add vanilla-extract plugin
       webpackConfig.plugins.push(new VanillaExtractPlugin());
+
       webpackConfig.resolve.symlinks = true;
+
       return webpackConfig;
     },
   },
